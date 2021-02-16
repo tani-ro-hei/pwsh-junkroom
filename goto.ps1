a1f2e4b3f5618c4919bc9a3d030f1001a4cbbc0b
tani-ro-hei <36533066+tani-ro-hei@users.noreply.github.com>
2021-02-16 13:46:38 +0900

    add 'next' feature to goto.ps1

--- goto.ps1
+++ goto.ps1
@@ -13,6 +13,12 @@ ElseIf ($Args[0] -eq "d") {
     Set-Location("C:" + $env:homepath + "\Dropbox\ws\otherLang\D")
 }
 
+# next なら 1 階層深く進む
+ElseIf ($Args[0] -eq "next") {
+     $dirs = Get-ChildItem * | Where-Object { $_.PSIsContainer }
+     cd $dirs[0]
+}
+
 # デフォルトではそのまま cd に渡す
 Else {
     cd $Args[0]
